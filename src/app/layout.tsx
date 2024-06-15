1bde1b4e55b5f66542e8e9d338af90e8c75f440e
vamshi_gujjuboina_7113 <vamshi.gujjuboina@3insys.com>
2024-06-15 16:57:35 +0530

    cursor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import '../styles/index.scss';
 // import ScrollToTop from '@/components/common/ScrollTop';
 import Footer from '@/app/footer/page';
 import Header from '@/components/Header/Header';
+import AnimatedCursor from "react-animated-cursor";
 
 if (typeof window !== 'undefined') {
   require('bootstrap/dist/js/bootstrap');
@@ -25,6 +26,29 @@ export default function RootLayout({ children }: IRootLayout) {
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet" />
       </head>
       <body>
+        <div className="cursor__dot">
+          <AnimatedCursor
+            innerSize={6}
+            outerSize={35}
+            color="255, 255 ,255"
+            outerAlpha={0.5}
+            innerScale={1}
+            outerScale={1}
+            outerStyle={{
+              borderRadius: "50%",
+              borderWidth: '2px',
+              borderStyle: 'solid',
+              borderImage: 'linear-gradient(to right, #0DCAF0, #6F42C1, #D63384) 1',
+              boxSizing: 'border-box',
+              width: "35px",
+              height: "35px",
+              backgroundClip: 'content-box'
+            }}
+            innerStyle={{
+              backgroundColor: "#0dcaf0"
+            }}
+          />
+        </div>
         <div className='main-page-wrapper'>
           <Header />
           {children}
